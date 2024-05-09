830529d5b50510f9a7d2da1ccc620eed868d093d
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-05-09 14:48:32 +0530

    BackendCommit

--- Backend/databse/dbConnection.js
+++ Backend/databse/dbConnection.js
@@ -4,7 +4,7 @@ export const dbConnection = () =>{
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "MERN_STACK_JOB_SEEKING"
     }).then(()=>{
-        console.log("Connected to database!");
+        console.log("Connected to database...");
     }).catch((err)=>{
         console.log(`Some error occured while connecting to database. ${err}`);
     });
