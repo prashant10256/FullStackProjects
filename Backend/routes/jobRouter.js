3c691c657ac01a3790f543b2501f500f96c7575a
Prashant_Sugave <95568247+prashant10256@users.noreply.github.com>
2024-05-11 14:52:16 +0530

    upload jobs

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { deleteJob, getAllJobs, getMyJobs, postJob, updateJob } from "../controllers/jobController.js";
+import { deleteJob, getAllJobs, getMyJobs, getSinglejob, postJob, updateJob } from "../controllers/jobController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -9,6 +9,9 @@ router.post("/post", isAuthenticated, postJob);
 router.get("/getmyjobs", isAuthenticated, getMyJobs);
 router.put("/update/:id", isAuthenticated, updateJob);
 router.delete("/delete/:id", isAuthenticated, deleteJob);
+router.get("/:id", isAuthenticated, getSinglejob);
+
+
 
 
 
--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { Context } from "../../main";
+import { Link, useNavigate } from "react-router-dom";
 
 const Jobs = () => {
-  return (
-    <div>Jobs</div>
-  )
+  const [jobs, setJobs] = useState([]);
+  const {} = useContext(Context);
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    try {
+      axios
+        .get("http://localhost:4000/api/v1/job/getall", {
+          withCredentials: true,
+        })
+        .then((res) => {
+          setJobs(res.data.jobs);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+if(!isAuthorized){
+  navigateTo("/login");
 }
 
-export default Jobs
+  return (
+    <>
+    <section className="jobs pages">
+      <div className="container">
+        <h1>ALL AVAILABLE JOBS</h1>
+        <div className="banner">
+          {
+            jobs.jobs && jobs.jobs.map((element)=>{
+              return(
+                <div className="card" key={element._id}>
+                  <p>{element.title}</p>
+                  <p>{element.category}</p>
+                  <p>{element.country}</p>
+                  <Link to={'/job/${element._id}'}>Job Details</Link>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
+    </>
+  );
+};
+
+export default Jobs;
